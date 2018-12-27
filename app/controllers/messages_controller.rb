7195c7dec60c0605dc017235c53a93e6c2ddc7c1
Fippiy <profier4@gmail.com>
2018-12-27 15:30:47 +0900

    Edit messages_controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,4 +5,16 @@ class MessagesController < ApplicationController
   def new
     @message = Message.new
   end
+
+  def create
+    Message.create(message_params)
+    redirect_to :root
+  end
+
+  private
+  def message_params
+    # binding.pry
+    params.require(:message).permit(:message).merge(user_id: current_user.id)
+    # .merge(current_user.id)
+  end
 end
