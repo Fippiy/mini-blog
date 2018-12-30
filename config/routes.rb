3144ce9f063e41da192614e486fbdb3fd31b7125
Fippiy <profier4@gmail.com>
2018-12-30 11:41:24 +0900

    Edit routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'messages#index'
   resources :messages do
-    resources :comments, only: :create
+    resources :comments, only: [:create, :destroy]
   end
   resources :users, only: :show
 end
