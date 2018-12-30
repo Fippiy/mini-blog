403cc8eb44ffbd7d7a8050f38faa8471bd00d04b
Fippiy <profier4@gmail.com>
2018-12-30 11:41:36 +0900

    Edit comments_controller.rb

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,6 +6,12 @@ class CommentsController < ApplicationController
     redirect_to message_path(@comment.message_id)
   end
 
+  def destroy
+    comment = Comment.find(params[:id])
+    comment.destroy if comment.user_id == current_user.id
+    redirect_to message_path(comment.message_id)
+  end
+
   private
   def comment_params
     params.require(:comment).permit(:comment).merge(message_id: params[:message_id],user_id: current_user.id)
